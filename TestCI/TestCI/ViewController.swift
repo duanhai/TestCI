e274e62548bfad8305a7cbb3d4283c2b11b9c02b
Tony Duan <duanhai@outlook.com>
2017-01-15 23:30:26 +0800

    add

---
+++ TestCI/TestCI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestCI
+//
+//  Created by Tony Duan on 2017/1/15.
+//  Copyright © 2017年 ChengduRuiMa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
